a1f568a23040651cb8a7a71dc819b41c1eb6ead9
Gustavo Tremont <gustavotremontsr@gmail.com>
2022-01-14 18:48:36 +0100

    create proxy and endpoit to client

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 /****************** Nodejs Dependencies ******************/
 const express = require('express')
 const logger = require('morgan');
+const path = require('path');
+require('dotenv').config();
 
 /****************** Project Dependencies ******************/
 
@@ -15,10 +17,13 @@ const port = process.env.PORT || 5000;
 /****************** Express Settings ******************/
 app.use(express.json()); //Para habilitar envio de JSON al servidor
 app.use(express.urlencoded( { extended: false } )); //Habilita la lectura del body por metodo post
+app.use(express.static(path.join(__dirname, 'client/build')));
 app.use(logger('dev')) // habilitar Morgan con preset dev
 
 /****************** Routes ******************/
-
+app.get('/', (req,res) =>{
+    res.sendFile(path.join(__dirname+'/client/build/index.html'));
+});
 
 /****************** Actice Server ******************/
 app.listen(port, () => {
