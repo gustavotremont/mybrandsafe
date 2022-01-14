813640ad998b2e1d61f9abcb6783261ae835679a
Gustavo Tremont <gustavotremontsr@gmail.com>
2022-01-14 19:03:09 +0100

    create sequelize migrations

---
+++ migrations/20220114175845-create-reports.js
@@ -0,0 +1,31 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, DataTypes) => {
+    await queryInterface.createTable('reports', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: DataTypes.INTEGER
+      },
+      uuid: {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4
+      },
+      dataReport: {
+        type: DataTypes.TEXT
+      },
+      createdAt: {
+        allowNull: false,
+        type: DataTypes.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: DataTypes.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('reports');
+  }
+};
