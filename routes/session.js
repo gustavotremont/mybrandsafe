c97ed2b8f021c9f8408a47234a6120fb67e46704
Gustavo Tremont <gustavotremontsr@gmail.com>
2022-01-17 00:25:32 +0100

    create session controllers and routes

---
+++ routes/session.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const routes = express.Router();
+const session = require('../controllers/session')
+
+routes.post('/session/login', (req, res, next) => {
+    session.login(req, res, next);
+}) 
+routes.post('/session/logout', session.logout)
+routes.post('/session/auth', session.authLogin)
+
+module.exports = routes;
