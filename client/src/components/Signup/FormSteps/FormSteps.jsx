194787d474b65779a6fa41c2174e7674519709a4
Gustavo Tremont <gustavotremontsr@gmail.com>
2022-01-20 03:53:34 +0100

    handle signup

--- client/src/components/Signup/FormSteps/FormSteps.jsx
+++ client/src/components/Signup/FormSteps/FormSteps.jsx
@@ -1,26 +1,6 @@
-import React, {useState} from "react";
+import React from "react";
 
-const FormSteps = ({steps, handleSignUp}) => {
-
-  const [formInputs, setFormInputs] = useState({
-    email: '',
-    password: '',
-    repeatPassword: '',
-    name: '',
-    address: '',
-    cif: '',
-    domains: '',
-    emails: '',
-    images: ''
-  });
-
-  const handleChange = (e) => {
-    const name = e.target.name;
-    setFormInputs({
-      ...formInputs,
-      [name]: e.target.value
-    })
-  }
+const FormSteps = ({steps, formInputs, handleChange}) => {
 
   const formStepCount = () => {
     switch(steps) {
@@ -71,7 +51,7 @@ const FormSteps = ({steps, handleSignUp}) => {
             </div>
 
             <div className="signup-body--field">
-              <label className="signup-body--field--label" htmlFor="emails">Correo</label>
+              <label className="signup-body--field--label" htmlFor="emails">Correo Empresarial</label>
               <input className="signup-body--field--input" value={formInputs.emails} onChange={handleChange} type="text" name="emails" id="signupEmails" placeholder="Introduzca el correo de su empresa" />
             </div>
           </>
@@ -92,18 +72,26 @@ const FormSteps = ({steps, handleSignUp}) => {
 
             <div className="signup-body--field">
               <label className="signup-body--field--label" htmlFor="signupProfile">Introduce tu perfil</label>
-              <input className="signup-body--field--input" onChange={handleChange} type="text" name="signupProfile" id="signupProfile" placeholder="Introduce tu perfil" />
+              <input className="signup-body--field--input" value={formInputs.profile} onChange={handleChange} type="text" name="profile" id="signupProfile" placeholder="Introduce tu perfil" />
             </div>
 
             <div className="signup-body--field">
               <span className="signup-body--field--label">Introduce tus Logos</span>
               <input className="imageFiles-input" type="file" onChange={handleChange} name="images" id="signupImage"/>
               <label className="imageFiles-label" htmlFor="images">
-                  Arrastra y suelta un archivo
-                  <span className="imageFiles-label--button" >
-                    <img className="file-icon" src="https://firebasestorage.googleapis.com/v0/b/mybrandsafe.appspot.com/o/pages-images%2Ficon-paperclip.png?alt=media&token=4ebcce9d-c2c8-4fa1-8f37-9c533fce5758" alt="file" />
-                    Selecciona tu archivo
-                  </span>
+                  {
+                    formInputs.images
+                      ? <>
+                            <img className="upload-file" src={URL.createObjectURL(formInputs.images)} alt="logo empresarial" />
+                        </>
+                      : <>
+                            Arrastra y suelta un archivo
+                            <span className="imageFiles-label--button" >
+                              <img className="file-icon" src="https://firebasestorage.googleapis.com/v0/b/mybrandsafe.appspot.com/o/pages-images%2Ficon-paperclip.png?alt=media&token=4ebcce9d-c2c8-4fa1-8f37-9c533fce5758" alt="file" />
+                              Selecciona tu archivo
+                            </span>
+                        </>
+                  }
               </label>
             </div>
           </>
--- client/src/components/Signup/Signup.jsx
+++ client/src/components/Signup/Signup.jsx
@@ -1,16 +1,77 @@
-import React, {useState} from "react";
+import React, {useState, useEffect, useContext} from "react";
+import { Link, useNavigate } from 'react-router-dom'
+import { sessionContext } from '../../context/sessionContext'
 import HeroSteps from './HeroSteps'
 import FormSteps from './FormSteps'
+import axios from 'axios'
+import { handleUpload } from '../../firestorage/firebase'
 
 const Signup = () => {
+  const navigate = useNavigate()
+  const {session} = useContext(sessionContext)
   const [step, setStep] = useState(0);
 
-  const handleSignUp = (signUpBody) => {
+  const [formInputs, setFormInputs] = useState({
+    email: '',
+    password: '',
+    repeatPassword: '',
+    name: '',
+    address: '',
+    cif: '',
+    domains: '',
+    emails: '',
+    images: '',
+    profile: ''
+  });
+
+  useEffect(() => {
+    if(session) navigate('/dashboard')
+  }, [session])
+
+  const handleChange = (e) => {
+    const name = e.target.name;
+
+    name === 'images'
+      ? setFormInputs({ ...formInputs, images: e.target.files[0] })
+      : setFormInputs({ ...formInputs, [name]: e.target.value })
     
+  }
+
+  const handleSignUp = async () => {
+    axios.post('/api/users', 
+      {
+        email: formInputs.email,
+        password: formInputs.password,
+        repeatPassword: formInputs.repeatPassword,
+        name: formInputs.name,
+        address: formInputs.address,
+        cif: formInputs.cif,
+      })
+      .then( async (res) => {
+
+        const fileUrl = await handleUpload(formInputs.images)
+
+        axios.post(`/api/assets/${res.data.user.uuid}`, 
+          {
+            domains: [formInputs.domains], 
+            emails: [formInputs.emails], 
+            images: [fileUrl]
+          })
+          .then( res => {
+            navigate('/login')
+          })
+          .catch( error => {
+            console.log(error.response.data.errors)
+          })
+      })
+      .catch( error => {
+        console.log(error.response.data.errors)
+      })
   }
 
   const nextStep = (e) => {
     e.preventDefault()
+    if (step === 3) handleSignUp()
     if (step < 3) setStep(step+1)
   }
   const backStep = (e) => {
@@ -21,7 +82,7 @@ const Signup = () => {
   return (
     <section className="flex-division">
       <section className="hero">
-          <h2 className="hero--logo" >My<strong className='hero--logo--altcolor'>Brand</strong>Safe</h2>
+          <Link to='/' className="hero--logo" >My<strong className='hero--logo--altcolor'>Brand</strong>Safe</Link>
           <div className="hero--info">
             <HeroSteps steps={step}/>
           </div>
@@ -31,10 +92,12 @@ const Signup = () => {
             <h3 className="signup-body--title" >Completa tu perfil</h3>
             <p className="signup-body--span">Introduce los datos de tu marca para poder realizar tu primer informe.</p>
             
-            <FormSteps steps={step} handleSignUp={handleSignUp}/>
+            <FormSteps steps={step} formInputs={formInputs} handleChange={handleChange}/>
 
             <button className="signup-body--button" onClick={nextStep} >Continuar</button>
             { step === 0 ? null : <button className="signup-body--button-back" onClick={backStep} >Atras</button> }
+
+            <span className="form-body--login" >Ya tienes cuenta ? <Link className="form-body--login--link" to='/login' >Entra Aquí</Link></span>
           </form>
       </section>
 
