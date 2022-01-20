98098c6c5fafae6ba552866f7940c5ff8c046548
Gustavo Tremont <gustavotremontsr@gmail.com>
2022-01-20 18:54:21 +0100

    create Loader component

--- client/src/components/Main/Main.jsx
+++ client/src/components/Main/Main.jsx
@@ -1,16 +1,20 @@
-import React from "react";
+import React, {useState} from "react";
 import { Routes, Route } from 'react-router-dom'
 import Landing from '../Landing'
 import Login from '../Login'
 import Signup from '../Signup'
+import Loader from '../Loader'
 
 const Main = () => {
+  const [Loading, setLoading] = useState(false);
+
   return (
     <main>
+      <Loader isLoading={Loading}/>
       <Routes>
         <Route path='/' element={ <Landing/> } />
-        <Route path='/login' element={ <Login/> } />
-        <Route path='/signup' element={ <Signup/> } />
+        <Route path='/login' element={ <Login isLoading={setLoading}/> } />
+        <Route path='/signup' element={ <Signup isLoading={setLoading}/> } />
         <Route path='/dashboard' element={ <div>Dashboard</div> } />
       </Routes>
     </main>
